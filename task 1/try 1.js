82758db723162be75d79b706e37af11fb8655fe9
adeArtup <adeplesmana04@gmail.com>
2020-02-17 21:19:07 +0700

    first commit

---
+++ task 1/try 1.js
@@ -0,0 +1,11 @@
+let message = "Perkenalkan Saya"
+let myName
+let whyChooseHacktiv8 = "karena awalnya adalah tuntutan pekerjaan untuk bisa membangun aplikasi berbasis mobile yang bisa di pakai di berbagai platform"
+
+let completeMessage
+myName = 'Ade Putra Lesmana'
+message = message + ' ' + myName
+
+completeMessage = message + ' ' + whyChooseHacktiv8
+
+console.log(completeMessage)
